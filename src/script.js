effc2a8c90c75a2da8e480468d642b1419108f6a
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2022-09-13 11:18:07 +0200

    initial commit

---
+++ src/script.js
@@ -0,0 +1,61 @@
+const numbers = document.querySelectorAll('.numbers')
+const specials = document.querySelectorAll('.special')
+const screen = document.querySelector('.screen')
+
+let equal = undefined
+let actualNumber, previousNumber = undefined
+
+numbers.forEach(number => {
+    number.addEventListener('click',(e) => {
+        screen.textContent += e.target.innerText
+    })
+})
+
+specials.forEach(element => {
+    element.addEventListener('click', (e) => {
+        const attribute = e.target.attributes[1].value
+
+        switch (attribute) {
+            case 'clear':
+                screen.textContent = ''
+                break;
+            case 'plus':
+                if(getLastChar()) {
+                    screen.textContent += ' + '
+                }
+                break;
+            case 'minus':
+                if(getLastChar()) {
+                    screen.textContent += ' - '
+                }
+                break;
+            case 'divide':
+                if(getLastChar()) {
+                    screen.textContent += ' / '
+                }
+                break;
+            case 'multiply':
+                if(getLastChar()) {
+                    screen.textContent += ' * '
+                }
+                break;
+            case 'equal':
+                screen.textContent = ''
+
+                screen.textContent = equal
+                break;
+        }
+    })
+})
+
+function getLastChar() {
+    const text = screen.textContent
+    if(text.endsWith(' + ') || text.endsWith(' - ') || text.endsWith(' / ')|| text.endsWith(' * ') || screen.textContent === '') {
+        return false
+    } else return true
+}
+
+function getNumber() {
+    actualNumber = screen.textContent
+}
+
